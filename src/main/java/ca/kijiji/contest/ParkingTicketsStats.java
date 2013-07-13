f42a49df4ea76cf51b0d05af593654cfd8fd42a7
Jonathan Fuerth <jfuerth@gmail.com>
2013-07-13 00:05:53 -0400

    Half-hearted first attempt at a concurrent solution. Uses one thread for I/O and another for parsing.
    As I feared, this is slower than the single-threaded implementation. I think my original solution
    was already quite I/O bound, so using concurrency to squeeze more performance out of it will be
    an uphill battle.

--- src/main/java/ca/kijiji/contest/ParkingTicketsStats.java
+++ src/main/java/ca/kijiji/contest/ParkingTicketsStats.java
@@ -6,9 +6,14 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.SortedMap;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -21,81 +26,47 @@ public class ParkingTicketsStats {
 
   private static final Logger LOG = LoggerFactory.getLogger(ParkingTicketsStats.class);
 
-  /**
-   * The part that comes before the street name in the street address. This part
-   * is the hairiest part of the data; it looks like it was keyed in manually on
-   * horrible keyboards by people who don't care about data quality. There are a
-   * lot of random characters in the street numbers, as well as lookalikes such
-   * as lowercase l standing in for the digit 1.
-   */
-  private static final String ADDRESS_PATTERN = "l?[*\"\\(\\)\\[\\]\\\\@:'=;0-9.!?%/#abc$_ -]";
-
-  /**
-   * Pattern for matching street names. This part of the data is quite clean and regular.
-   * It looks like it was entered by choosing from a list of official street names.
-   */
-  private static final String STREET_NAME_PATTERN = "[a-zA-Z]+?[a-zA-Z0-9 '-]+";
-
-  /**
-   * All Toronto street types according to the readme file in the
-   * <a href="http://opendata.toronto.ca/gcc/centreline_wgs84.zip">Toronto Centreline dataset</a>.
-   * <p>
-   * Note: I added the following nonstandard abbreviations because they were common in
-   * the parking ticket data set:
-   * <ul>
-   *  <li>AV
-   *  <li>CIR
-   * </ul>
-   */
-  private static final String STREET_TYPES_PATTERN =
-          "(AVENUE|AVE?|BRIDGE|BDGE|BOULEVARD|BLVD|CIRC?L?E?|CRCL|COURT|CRT|CIRCUIT|CRCT|CRESENT|CRES|" +
-          "CLOSE|CS|DRIVE|DR|EXPRESSWAY|XWY|GARDEN|GDN|GARDENS|GDNS|GATE|GT|GREEN|GRN|GROVE|GRV|" +
-          "HEIGHTS|HTS|HIGHWAY|HWY|HILL|HILL|LANE|LANE|LAWN|LWN|LINE|LINE|MEWS|MEWS|PARK|PK|" +
-          "PARKWAY|PKWY|PATH|PATH|PATHWAY|PTWY|PLACE|PL|PROMENADE|PROM|RAMP|RAMP|ROAD|RD|ROADWAY|RDWY|" +
-          "SQUARE|SQ|STREET|ST|TERRACE|TER|TRAIL|TRL|VIEW|VIEW|WALK|WALK|WAY|WAY|WOODS|WDS)";
-
-  public static SortedMap<String, Integer> sortStreetsByProfitability(InputStream parkingTicketsStream) throws IOException {
+  public static SortedMap<String, Integer> sortStreetsByProfitability(InputStream parkingTicketsStream) throws IOException, InterruptedException, ExecutionException {
     BufferedReader in = new BufferedReader(new InputStreamReader(parkingTicketsStream, "ascii"));
-    Map<String, Map.Entry<String, Integer>> ticketsByStreet = new HashMap<String, Map.Entry<String, Integer>>();
-
-    final Pattern addressPattern = Pattern.compile(
-            "(" + ADDRESS_PATTERN + ")*" +        // optional "numeric" address
-            " *(" + STREET_NAME_PATTERN + "?)" +  // the street name itself (this is what we're trying to capture)
-            "( " + STREET_TYPES_PATTERN + ")?" +  // the street type. this really helps anchor the street names
-            "( [NSEW].*)?" +                      // optional street direction
-            "\\.?" +                              // some entries end with a helpful '.'
-            "( UNIT #[0-9]+)?");                  // about 50 entries include an apartment number
 
     // this skips the header line in the data
     in.readLine();
 
-    int badAddressCount = 0;
-    int lineCount = 0;
-    for (String line = in.readLine(); line != null; line = in.readLine()) {
-      String[] fields = line.split(",");
-      int amount = Integer.parseInt(fields[4]);
-      String address = fields[7];
-      Matcher matcher = addressPattern.matcher(address);
-      if (matcher.matches()) {
-        String street = matcher.group(2);
-        StreetAmount ticket = (StreetAmount) ticketsByStreet.get(street);
-        if (ticket == null) {
-          ticket = new StreetAmount(amount, street);
-          ticketsByStreet.put(street, ticket);
-        }
-        else {
-          ticket.add(amount);
-        }
-      }
-      else {
-        LOG.debug("Bad address: {}", address);
-        badAddressCount++;
-      }
-      lineCount++;
-    }
-    LOG.info(badAddressCount + " bad addresses (" + (100.0 * badAddressCount / lineCount) + "%)");
+    final ConcurrentLinkedQueue<String> lines = new ConcurrentLinkedQueue<String>();
 
-    SortedMap<String, Integer> result = new SortedByValueMap<String, Integer>(ticketsByStreet.values());
+    Callable<Map<String, Map.Entry<String, Integer>>> parseWorker = new Callable<Map<String,Map.Entry<String,Integer>>>() {
+
+      public Map<String, Map.Entry<String, Integer>> call() throws Exception {
+        Map<String, Map.Entry<String, Integer>> myResults = new HashMap<String, Map.Entry<String, Integer>>();
+        String line;
+        while ((line = lines.poll()) != null) {
+          try {
+            Entry<String, Integer> amount = StreetAmount.from(line);
+            Entry<String, Integer> existingAmount = myResults.get(amount.getKey());
+            if (existingAmount != null) {
+              existingAmount.setValue(existingAmount.getValue() + amount.getValue());
+            }
+            else {
+              myResults.put(amount.getKey(), amount);
+            }
+          }
+          catch (IllegalArgumentException e) {
+            // bad address
+            // FIXME better exception type
+          }
+        }
+        return myResults;
+      }
+    };
+
+    ExecutorService threadPool = Executors.newFixedThreadPool(4);
+    Future<Map<String, Entry<String, Integer>>> parseResults = threadPool.submit(parseWorker);
+
+    for (String line = in.readLine(); line != null; line = in.readLine()) {
+      lines.add(line);
+    }
+
+    SortedMap<String, Integer> result = new SortedByValueMap<String, Integer>(parseResults.get().values());
 
     if (LOG.isDebugEnabled()) {
       for (Map.Entry<String, Integer> entry : result.entrySet()) {
--- src/main/java/ca/kijiji/contest/StreetAmount.java
+++ src/main/java/ca/kijiji/contest/StreetAmount.java
@@ -1,8 +1,51 @@
 package ca.kijiji.contest;
 
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public final class StreetAmount implements Map.Entry<String, Integer> {
+
+  /**
+   * The part that comes before the street name in the street address. This part
+   * is the hairiest part of the data; it looks like it was keyed in manually on
+   * horrible keyboards by people who don't care about data quality. There are a
+   * lot of random characters in the street numbers, as well as lookalikes such
+   * as lowercase l standing in for the digit 1.
+   */
+  private static final String ADDRESS_PATTERN = "l?[*\"\\(\\)\\[\\]\\\\@:'=;0-9.!?%/#abc$_ -]";
+
+  /**
+   * Pattern for matching street names. This part of the data is quite clean and regular.
+   * It looks like it was entered by choosing from a list of official street names.
+   */
+  private static final String STREET_NAME_PATTERN = "[a-zA-Z]+?[a-zA-Z0-9 '-]+";
+
+  /**
+   * All Toronto street types according to the readme file in the
+   * <a href="http://opendata.toronto.ca/gcc/centreline_wgs84.zip">Toronto Centreline dataset</a>.
+   * <p>
+   * Note: I added the following nonstandard abbreviations because they were common in
+   * the parking ticket data set:
+   * <ul>
+   *  <li>AV
+   *  <li>CIR
+   * </ul>
+   */
+  private static final String STREET_TYPES_PATTERN =
+          "(AVENUE|AVE?|BRIDGE|BDGE|BOULEVARD|BLVD|CIRC?L?E?|CRCL|COURT|CRT|CIRCUIT|CRCT|CRESENT|CRES|" +
+          "CLOSE|CS|DRIVE|DR|EXPRESSWAY|XWY|GARDEN|GDN|GARDENS|GDNS|GATE|GT|GREEN|GRN|GROVE|GRV|" +
+          "HEIGHTS|HTS|HIGHWAY|HWY|HILL|HILL|LANE|LANE|LAWN|LWN|LINE|LINE|MEWS|MEWS|PARK|PK|" +
+          "PARKWAY|PKWY|PATH|PATH|PATHWAY|PTWY|PLACE|PL|PROMENADE|PROM|RAMP|RAMP|ROAD|RD|ROADWAY|RDWY|" +
+          "SQUARE|SQ|STREET|ST|TERRACE|TER|TRAIL|TRL|VIEW|VIEW|WALK|WALK|WAY|WAY|WOODS|WDS)";
+
+  private static final Pattern FULL_PATTERN = Pattern.compile(
+          "(" + ADDRESS_PATTERN + ")*" +        // optional "numeric" address
+          " *(" + STREET_NAME_PATTERN + "?)" +  // the street name itself (this is what we're trying to capture)
+          "( " + STREET_TYPES_PATTERN + ")?" +  // the street type. this really helps anchor the street names
+          "( [NSEW].*)?" +                      // optional street direction
+          "\\.?" +                              // some entries end with a helpful '.'
+          "( UNIT #[0-9]+)?");                  // about 50 entries include an apartment number
 
   private int amount;
   private final String street;
@@ -16,8 +59,24 @@ public final class StreetAmount implements Map.Entry<String, Integer> {
     this.street = location;
   }
 
-  public void add(int amount) {
-    this.amount += amount;
+  /**
+   * Creates a StreetAmount instance by parsing the given line of parking ticket data.
+   *
+   * @param line
+   * @return
+   */
+  public static StreetAmount from(String line) {
+    String[] fields = line.split(",");
+    int amount = Integer.parseInt(fields[4]);
+    String address = fields[7];
+    Matcher matcher = FULL_PATTERN.matcher(address);
+    if (matcher.matches()) {
+      String street = matcher.group(2);
+      return new StreetAmount(amount, street);
+    }
+    else {
+      throw new IllegalArgumentException("Bad address: " + address);
+    }
   }
 
   @Override
@@ -36,6 +95,8 @@ public final class StreetAmount implements Map.Entry<String, Integer> {
   }
 
   public Integer setValue(Integer value) {
-    throw new UnsupportedOperationException();
+    Integer oldValue = amount;
+    this.amount = value;
+    return oldValue;
   }
 }
