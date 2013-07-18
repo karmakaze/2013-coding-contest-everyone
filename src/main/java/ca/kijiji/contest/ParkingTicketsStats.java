97c8015207c973a8ed571f5d3701c723eb8cb894
Darren Thompson <dthompson91@gmail.com>
2013-07-17 21:37:21 -0400

    Inital Commit

--- src/main/java/ca/kijiji/contest/ParkingTicketsStats.java
+++ src/main/java/ca/kijiji/contest/ParkingTicketsStats.java
@@ -1,11 +1,18 @@
 package ca.kijiji.contest;
 
 import java.io.InputStream;
+import java.io.IOException;
 import java.util.SortedMap;
 
 public class ParkingTicketsStats {
 
     public static SortedMap<String, Integer> sortStreetsByProfitability(InputStream parkingTicketsStream) {
+        try {
+          System.out.println(parkingTicketsStream.available());
+        }
+        catch(IOException exception){
+            return null;
+        }
         return null;
     }
 }
