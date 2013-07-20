0b8a3935c9b58c6d63cbe2d2fd0507bcd52c6158
Devin Lafreniere <devin@devsoftweb.com>
2013-07-20 11:32:26 -0400

    converted to scala

---
+++ src/main/scala/ca/kijiji/contest/ParkingTicketsStats.scala
@@ -0,0 +1,7 @@
+package ca.kijiji.contest
+
+object ParkingTicketsStats {
+  def sortStreetsByProfitability(parkingTicketsStream: java.io.InputStream): java.util.SortedMap[java.lang.String, java.lang.Integer] = {
+    return new java.util.TreeMap[java.lang.String, java.lang.Integer]
+  }
+}
