87d57fe1188c0a55e8f9b464395e4b6ad00da271
Justin Levine <justin.c.levine@gmail.com>
2013-07-20 18:46:33 -0400

    Simple reading of CSV abd print out fields.
    - Add CSV Reader dependency.
    - Simple reading of csv and print out fields.
    - New CSV Named Row class.
    -Update .gitignore.

---
+++ src/main/java/ca/kijiji/contest/CSVNamedRow.java
@@ -0,0 +1,22 @@
+package ca.kijiji.contest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CSVNamedRow {
+	private Map<String, Integer> _fieldNameToIndex = new HashMap<String, Integer>();
+	
+	public CSVNamedRow(String[] headers) {
+		for (int i = 0; i < headers.length; ++i) {
+			_fieldNameToIndex.put(headers[i], i);
+		}		
+	}
+
+	public String getField(String[] row, String fieldName) {
+		return row[_fieldNameToIndex.get(fieldName)];
+	}
+	
+	public String toString() {
+		return _fieldNameToIndex.toString();
+	}
+}
--- src/main/java/ca/kijiji/contest/ParkingTicketsStats.java
+++ src/main/java/ca/kijiji/contest/ParkingTicketsStats.java
@@ -1,11 +1,41 @@
 package ca.kijiji.contest;
 
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.SortedMap;
+import java.util.TreeMap;
+
+import au.com.bytecode.opencsv.CSVReader;
 
 public class ParkingTicketsStats {
 
-    public static SortedMap<String, Integer> sortStreetsByProfitability(InputStream parkingTicketsStream) {
-        return null;
+	public static final String FINE_AMOUNT_FIELD_NAME = "set_fine_amount";
+	public static final String LOCATION_FIELD_NAME = "location2";
+	
+    public static SortedMap<String, Integer> sortStreetsByProfitability(InputStream parkingTicketsStream) throws IOException {
+		SortedMap<String, Integer> streetToFeeSum = new TreeMap<String, Integer>();
+		
+		CSVReader ticketReader = new CSVReader(new InputStreamReader(parkingTicketsStream));
+		
+		String[] ticketData = null;
+		
+		CSVNamedRow namedRow = new CSVNamedRow(ticketReader.readNext());
+		System.out.println(namedRow);
+		int counter = 0;
+		while ((ticketData = ticketReader.readNext()) != null) {
+			System.out.println(namedRow.getField(ticketData, LOCATION_FIELD_NAME));
+			System.out.println(namedRow.getField(ticketData, FINE_AMOUNT_FIELD_NAME));
+			if (counter == 15) {
+				break;
+			}
+			++counter;
+		}
+		
+		ticketReader.close();
+//		int fineAmount = Integer.parseInt(ticket.get(4));
+//		String location = ticket.get(7);
+//		String street = StreetParser.parseStreet(location);
+        return streetToFeeSum;
     }
-}
+}
