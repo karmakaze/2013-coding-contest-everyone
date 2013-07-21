3ca20fbb863470e7f32cf3844e8ed99fe5921295
Justin Levine <jclevine.receipts@gmail.com>
2013-07-21 04:23:25 -0400

    Hack for getting largest sum from map sorted by keys.

--- src/main/java/ca/kijiji/contest/SortedCountMap.java
+++ src/main/java/ca/kijiji/contest/SortedCountMap.java
@@ -6,6 +6,8 @@ import java.util.TreeMap;
 public class SortedCountMap<K, V> extends TreeMap<String, Integer> {
 
 	private static final long serialVersionUID = -6233514320502923598L;
+	String largestFineSumStreet = null;
+	int largestFineSum = 0;
 	
 	@Override
     public Integer put(String key, Integer incrementValue) {
@@ -14,7 +16,18 @@ public class SortedCountMap<K, V> extends TreeMap<String, Integer> {
         	insertValue = 0;            
         }
         insertValue += incrementValue;
+        
+        if (insertValue > largestFineSum) {
+        	largestFineSum = insertValue;
+        	largestFineSumStreet = key;
+        }
+        
         super.put(key, insertValue);
         return insertValue;
-    }    
+    }   
+	
+	// TODO: Is this really the best way? Ugh.
+	public String firstKey() {
+		return largestFineSumStreet;
+	}
 }	
