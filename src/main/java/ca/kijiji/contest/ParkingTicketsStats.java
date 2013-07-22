0435a74e689b4c0318a98b170475d5cc3bd9532c
Justin Levine <jclevine.receipts@gmail.com>
2013-07-22 19:40:32 -0400

    Use far simpler algorithm to parse street.

--- src/main/java/ca/kijiji/contest/ParkingTicketsStats.java
+++ src/main/java/ca/kijiji/contest/ParkingTicketsStats.java
@@ -3,8 +3,10 @@ package ca.kijiji.contest;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.Charset;
+import java.util.Arrays;
 import java.util.SortedMap;
 
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -23,7 +25,6 @@ public class ParkingTicketsStats {
 	private static final Logger LOG = LoggerFactory.getLogger(ParkingTicketsStats.class);
 	public static final String FINE_AMOUNT_FIELD_NAME = "set_fine_amount";
 	public static final String LOCATION_FIELD_NAME = "location2";
-
 
 	/**
 	 * Returns the map of streets to fines.
@@ -63,96 +64,34 @@ public class ParkingTicketsStats {
     }
     
     /**
-     * Takes a location (combination of optional number, street, suffix, and optional direction 
+     * Takes a location (combination of optional number, street, suffix, and optional direction) 
      * and parses out the street name from it.
      * @param location The full location.
      * @return Just the street name.
      * @throws UnparseableLocationException Unable to determine the street from the location.
      */
     public static String parseStreet(String location) throws UnparseableLocationException {
-    	
-    	// Special case for those weird streets.
-        if (location == "THE QUEENSWAY" || location == "THE KINGSWAY" ||
-                location == "THE WEST MALL" || location == "THE EAST MALL") {
-                return location;
-        }
-        String[] locationParts = location.split(" ");
-        int numLocationParts = locationParts.length;
-
-        // 5 parts
-        boolean hasAllPartsPlusTwoWordStreetName = numLocationParts == 5 &&
-        										   SuffixDirectionEquilizer.isSuffix(locationParts[3]) &&
-        										   SuffixDirectionEquilizer.isDirection(locationParts[4]);
-        
-        // 4 parts
-        boolean isAMall = numLocationParts == 4 &&
-		          (String.format("%s %s %s", locationParts[1], locationParts[2], locationParts[3])
-		           .equals("THE EAST MALL") ||
-		           String.format("%s %s %s", locationParts[1], locationParts[2], locationParts[3])
-		           .equals("THE WEST MALL"));
-
-		boolean hasNumStreetSuffixAndDirection = numLocationParts == 4 && 
-												 Character.isDigit(locationParts[0].charAt(0)) &&
-        										 SuffixDirectionEquilizer.isSuffix(locationParts[2]) &&
-        										 SuffixDirectionEquilizer.isDirection(locationParts[3]);
-        
-        boolean hasNumTwoWordStreetAndSuffix = numLocationParts == 4 &&
-        									   SuffixDirectionEquilizer.isSuffix(locationParts[3]);
-        
-        boolean hasTwoWordStreetSuffixAndDirection = numLocationParts == 4 &&
-        											 !Character.isDigit(locationParts[0].charAt(0)) &&
-        											 SuffixDirectionEquilizer.isSuffix(locationParts[2]) &&
-            										 SuffixDirectionEquilizer.isDirection(locationParts[3]);
-        
-        // 3 parts
-        boolean hasNumAndSpecialWays = numLocationParts == 3 &&
-        							   (String.format("%s %s", locationParts[1], locationParts[2]).equals("THE QUEENSWAY") ||
-        							    String.format("%s %s", locationParts[1], locationParts[2]).equals("THE KINGSWAY"));
-        
-        boolean hasNumStreetAndSuffix = numLocationParts == 3 &&
-        								Character.isDigit(locationParts[0].charAt(0));
-        
-        boolean hasStreetSuffixAndDirection = numLocationParts == 3 &&
-											  !Character.isDigit(locationParts[0].charAt(0));
-        
-        boolean hasTwoWordStreetAndSuffix = numLocationParts == 3 &&
-        								    !Character.isDigit(locationParts[0].charAt(0)) &&
-        									SuffixDirectionEquilizer.isSuffix(locationParts[2]); 
-        
-        // 2 parts
-        boolean hasStreetAndSuffix = numLocationParts == 2 &&
-        							 SuffixDirectionEquilizer.isSuffix(locationParts[1]);
-        
-        boolean hasTwoWordStreetOnly = numLocationParts == 2 &&
-        							   !SuffixDirectionEquilizer.isSuffix(locationParts[1]);
-        
-        // 1 part
-        boolean hasStreetOnly = numLocationParts == 1;
-        
-        if (hasAllPartsPlusTwoWordStreetName) {        	
-    		// Get the 2 word street name
-    		return String.format("%s %s", locationParts[1], locationParts[2]);
+    	if (location.equals("")) {
+    		throw new UnparseableLocationException(location);
     	}
-        else if (isAMall) {
-        	return String.format("%s %s %s", locationParts[1], locationParts[2], locationParts[3]);
-        }
-        else if (hasNumStreetSuffixAndDirection) {
-        	return locationParts[1];
-        }
-        else if (hasNumTwoWordStreetAndSuffix || hasNumAndSpecialWays) {
-        	return String.format("%s %s", locationParts[1], locationParts[2]);
-        }
-        else if (hasNumStreetAndSuffix) {
-        	return locationParts[1];
-        }
-        else if (hasStreetSuffixAndDirection || hasStreetAndSuffix || hasStreetOnly) {
-        	return locationParts[0];
-        }
-        else if (hasTwoWordStreetSuffixAndDirection || hasTwoWordStreetOnly || hasTwoWordStreetAndSuffix) {
-        	return String.format("%s %s", locationParts[0], locationParts[1]);
-        }
-        else {
-        	throw new UnparseableLocationException(location);
-        }
+    	
+    	String[] locationParts = location.split(" ");
+    	
+    	boolean hasDirection = SuffixDirectionEquilizer.isDirection(locationParts[locationParts.length - 1]);
+    	if (hasDirection) {
+    		locationParts = Arrays.copyOfRange(locationParts, 0, locationParts.length - 1);
+    	}
+
+    	boolean hasSuffix = SuffixDirectionEquilizer.isSuffix(locationParts[locationParts.length - 1]);
+		if (hasSuffix) {
+			locationParts = Arrays.copyOfRange(locationParts, 0, locationParts.length - 1);
+		}
+    	
+		boolean hasNumber = Character.isDigit(locationParts[0].charAt(0));
+		if (hasNumber) {
+			locationParts = Arrays.copyOfRange(locationParts, 1, locationParts.length);
+		}
+		
+    	return StringUtils.join(locationParts, " ");
     }
 }
