8c2abe5ea2be5876ae83d5c3eb6f527eab38774e
Keith Kim <kkim@oanda.com>
2013-07-23 03:48:19 -0400

    Working ~1.2 sec on i7-2600K

--- src/main/java/ca/kijiji/contest/ParkingTicketsStats.java
+++ src/main/java/ca/kijiji/contest/ParkingTicketsStats.java
@@ -1,16 +1,17 @@
 package ca.kijiji.contest;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
 import java.util.Comparator;
 import java.util.Map;
 import java.util.SortedMap;
 import java.util.TreeMap;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicIntegerArray;
+import java.util.concurrent.atomic.AtomicReferenceArray;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -21,9 +22,214 @@ public class ParkingTicketsStats {
 	static final int UNUSED_BITS = 32 - BITS;
 	static final int SIZE = 1 << BITS;
 	static final int MASK = SIZE - 1;
-	static final String[] keys = new String[SIZE];
+	static final AtomicReferenceArray<String> keys = new AtomicReferenceArray<String>(SIZE);
 	static final AtomicIntegerArray vals = new AtomicIntegerArray(SIZE);
-	static byte[] data;
+	static volatile byte[] data;
+
+	static final String name = "([A-Z][A-Z][A-Z]+|ST [A-Z][A-Z][A-Z]+)";
+	static final Pattern namePattern = Pattern.compile(name);
+
+	static volatile int available;
+
+	static final ArrayBlockingQueue<Long> byteArrayQueue = new ArrayBlockingQueue<Long>(1024, true);
+	static final AtomicInteger workItems = new AtomicInteger();
+
+    public static SortedMap<String, Integer> sortStreetsByProfitability(InputStream parkingTicketsStream) {
+    	printInterval("Pre-entry initialization");
+/*
+		printProperty("os.arch");
+    	println("InputStream is "+ parkingTicketsStream);
+    	if (parkingTicketsStream instanceof BufferedInputStream) {
+    		BufferedInputStream bis = (BufferedInputStream) parkingTicketsStream;
+    	}
+*/
+    	try {
+			available = parkingTicketsStream.available();
+    		println("Bytes available: "+ available);
+
+			ThreadGroup group = new ThreadGroup("workers");
+			Runnable runnable = new Runnable() {
+				public void run() {
+					worker();
+				}};
+			int n = 5;
+			Thread[] threads = new Thread[n];
+			for (int k = 0; k < n; k++) {
+				threads[k] = new Thread(group, runnable, Integer.toString(k), 16 * 1024);
+			}
+
+    		data = new byte[available];
+
+    		workItems.incrementAndGet(); // 1 for first producer task
+
+    		for (Thread t : threads) {
+	    		t.start();
+    		}
+
+    		int a = 0;
+    		int i = 0;
+    		int j = 0;
+    		for (int c = 16 * 1024 * 1024; (c = parkingTicketsStream.read(data, a, c)) > 0; ) {
+    			a += c;
+    			workItems.incrementAndGet();
+    			i = j;
+    			j = a;
+    			if (a < available) {
+    				while (data[--j] != '\n') {}
+        			j++;
+    			}
+
+    			// don't offer the first (header) row
+    			if (i == 0) {
+    		    	printInterval("Local initialization: read first "+ a +" bytes");
+
+    				while (data[i++] != '\n') {};
+    			}
+
+    			long ij = (long)i << 32 | (long)j & 0x0ffffffffL;
+    			try {
+					while (!byteArrayQueue.offer(ij, 1, TimeUnit.SECONDS)) {}
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+
+    			if (available - a < c) {
+    				c = available - a;
+    			}
+    		}
+
+    		workItems.decrementAndGet();
+
+	    	printInterval("Local initialization: read remaining of "+ a +" total bytes");
+
+	    	for (Thread t: threads) {
+	    		try {
+					t.join();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+	    	}
+
+	    	printInterval("All worker threads completed");
+    	}
+    	catch (IOException e) {
+			e.printStackTrace();
+		}
+
+    	printInterval("Read and summed");
+
+//    	println("Size: "+ streets.size());
+
+    	SortedMap<String, Integer> sorted = new TreeMap<String, Integer>(new Comparator<String>() {
+			public int compare(String o1, String o2) {
+				int c = get(o2) - get(o1);
+				if (c != 0) return c;
+				return o2.compareTo(o1);
+			}});
+
+    	for (int i = 0; i < SIZE; i++) {
+    		int v = vals.get(i);
+    		if (v != 0) {
+    			sorted.put(keys.get(i), v);
+    		}
+    	}
+
+    	printInterval("Populated TreeSet");
+
+        return sorted;
+    }
+
+    /**
+     * worker parallel worker takes blocks of bytes read and processes them
+     */
+    static final void worker() {
+		try {
+			String threadName = Thread.currentThread().getName();
+			Matcher nameMatcher = namePattern.matcher("");
+
+			// local access faster than volatile fields
+			byte[] data = ParkingTicketsStats.data;
+
+			int work = 0;
+			do {
+				Long ij = byteArrayQueue.poll(30, TimeUnit.MILLISECONDS);
+				if (ij != null) {
+					int i = (int) (ij >>> 32);
+					int j = (int) (long) ij;
+				//	println("Thread ["+ threadName +"] processing block("+ i +", "+ j +")");
+
+					// process block
+					while (i < j) {
+						int m = i;
+						while (data[m++] != '\n' && m < j) {}
+						String line = new String(data, i, m - i);
+						i = m;
+
+			    		String[] parts = line.split(",");
+			    		try {
+	//			    		String tag_number_masked = parts[0];
+	//			    		String date_of_infraction = parts[1];
+	//			    		String infraction_code = parts[2];
+	//			    		String infraction_description = parts[3];
+				    		String sfa = parts[4];
+				    		Integer set_fine_amount = 0;
+				    		try {
+					    		set_fine_amount = Integer.parseInt(sfa);
+				    		}
+				    		catch (NumberFormatException e) {
+				    			System.out.print(e.getClass().getSimpleName() +": "+ sfa);
+				    		}
+	//			    		String time_of_infraction = parts[5];
+	//			    		String location1 = parts[6];
+				    		String location2 = parts[7];
+	//			    		String location3 = parts[8];
+	//			    		String location4 = parts[9];
+				    		nameMatcher.reset(location2);
+				    		if (nameMatcher.find()) {
+				    			String l = nameMatcher.group();
+	//		    			streetMatcher.reset(location2);
+	//		    			if (streetMatcher.find()) {
+	//		    				String l = streetMatcher.group(2);
+			    			/*
+					    	//	l = l.replaceAll("[0-9]+", "");
+					    		l = l.replaceAll("[^A-Z]+ ", "");
+					    		l = l.replaceAll(" (N|NORTH|S|SOUTH|W|WEST|E|EAST)$", "");
+					    		l = l.replaceAll(" (AV|AVE|AVENUE|BLVD|CRES|COURT|CRT|DR|RD|ST|STR|STREET|WAY)$", "");
+					    	//	l = l.replaceAll("^(A|M) ", "");
+					    		l = l.replaceAll("(^| )(PARKING) .*$", "");
+					    		l = l.trim();
+				    		*/
+	//				    		String province = parts[10];
+				    			add(l, set_fine_amount);
+	
+	//			    			if (!l.equals("KING") && (location2.indexOf(" KING ") >= 0 || location2.endsWith(" KING"))) {
+	//			    				println(l +" <- "+ location2);
+	//			    			}
+			    			}
+			    			else {
+			    				if (location2.indexOf("KING") >= 0 && location2.indexOf("PARKING") == -1) {
+				    				println(""+ location2);
+			    				}
+			    			}
+			    		}
+			    		catch (ArrayIndexOutOfBoundsException e) {
+			    			println(e.getClass().getSimpleName() +": "+ line);
+			    			e.printStackTrace();
+			    		}
+					}
+					work = workItems.decrementAndGet();
+				}
+				else {
+					work = workItems.get();
+				}
+			//	println("Thread ["+ threadName +"] work remaining "+ work +" queued="+ byteArrayQueue.size());
+			} while (work > 0);
+			println("Thread ["+ threadName +"] ending normally");
+		}
+		catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+    }
 
 	public static int hash(final String k) {
 		int h = 0;
@@ -45,10 +251,10 @@ public class ParkingTicketsStats {
 
 //		String k0 = keys[i];
 //		if (k0 != null && !k0.equals(k)) {
-//			System.err.println("Key hash clash: first "+ k0 +" and "+ k);
+//			println("Key hash clash: first "+ k0 +" and "+ k);
 //		}
 //		else {
-			keys[i] = k;
+			keys.set(i, k);
 //		}
 	}
 	public static int get(final String k) {
@@ -56,135 +262,23 @@ public class ParkingTicketsStats {
 		return vals.get(i);
 	}
 
-    public static SortedMap<String, Integer> sortStreetsByProfitability(InputStream parkingTicketsStream) {
-    	printInterval("Pre-entry initialization");
-
-    	String name = "([A-Z][A-Z][A-Z]+|ST [A-Z][A-Z][A-Z]+)";
-		Matcher nameMatcher = Pattern.compile(name).matcher("");
-
-		ThreadGroup group = new ThreadGroup("workers");
-		Runnable runnable = new Runnable() {
-			public void run() {
-			}};
-		Thread t1 = new Thread(group, runnable);
-		Thread t2 = new Thread(group, runnable);
-
-		printProperty("os.arch");
-    	System.out.println("InputStream is "+ parkingTicketsStream);
-    	if (parkingTicketsStream instanceof BufferedInputStream) {
-    		BufferedInputStream bis = (BufferedInputStream) parkingTicketsStream;
-    	}
-
-    	//final Map<String, Integer> streets = new HashMap<String, Integer>();
-    	try {
-    		int available = parkingTicketsStream.available();
-    		System.out.println("Bytes available: "+ available);
-    		data = new byte[available];
-    		int a = 0;
-    		for (int c = 16 * 4096; (c = parkingTicketsStream.read(data, a, c)) > 0; ) {
-    			a += c;
-    			if (available - a < c) {
-    				c = available - a;
-    			}
-    		}
-
-	    	printInterval("Local initialization: read "+ a +" bytes");
-
-	    	BufferedReader r = new BufferedReader(new InputStreamReader(parkingTicketsStream));
-	    	r.readLine();   // discard header row
-
-	    	for(String line; (line = r.readLine()) != null; ) {
-	    		String[] parts = line.split(",");
-	    		try {
-//		    		String tag_number_masked = parts[0];
-//		    		String date_of_infraction = parts[1];
-//		    		String infraction_code = parts[2];
-//		    		String infraction_description = parts[3];
-		    		String sfa = parts[4];
-		    		Integer set_fine_amount = 0;
-		    		try {
-			    		set_fine_amount = Integer.parseInt(sfa);
-		    		}
-		    		catch (NumberFormatException e) {
-		    			System.out.print(e.getClass().getSimpleName() +": "+ sfa);
-		    		}
-//		    		String time_of_infraction = parts[5];
-//		    		String location1 = parts[6];
-		    		String location2 = parts[7];
-//		    		String location3 = parts[8];
-//		    		String location4 = parts[9];
-		    		nameMatcher.reset(location2);
-		    		if (nameMatcher.find()) {
-		    			String l = nameMatcher.group();
-//	    			streetMatcher.reset(location2);
-//	    			if (streetMatcher.find()) {
-//	    				String l = streetMatcher.group(2);
-	    			/*
-			    	//	l = l.replaceAll("[0-9]+", "");
-			    		l = l.replaceAll("[^A-Z]+ ", "");
-			    		l = l.replaceAll(" (N|NORTH|S|SOUTH|W|WEST|E|EAST)$", "");
-			    		l = l.replaceAll(" (AV|AVE|AVENUE|BLVD|CRES|COURT|CRT|DR|RD|ST|STR|STREET|WAY)$", "");
-			    	//	l = l.replaceAll("^(A|M) ", "");
-			    		l = l.replaceAll("(^| )(PARKING) .*$", "");
-			    		l = l.trim();
-		    		*/
-//			    		String province = parts[10];
-		    			add(l, set_fine_amount);
-
-//		    			if (!l.equals("KING") && (location2.indexOf(" KING ") >= 0 || location2.endsWith(" KING"))) {
-//		    				System.out.println(l +" <- "+ location2);
-//		    			}
-	    			}
-	    			else {
-	    				if (location2.indexOf("KING") >= 0 && location2.indexOf("PARKING") == -1) {
-		    				System.out.println(""+ location2);
-	    				}
-	    			}
-	    		}
-	    		catch (ArrayIndexOutOfBoundsException e) {
-	    			System.out.println(e.getClass().getSimpleName() +": "+ line);
-	    		}
-	    	}
-    	}
-    	catch (IOException e) {
-			e.printStackTrace();
-		}
-
-    	printInterval("Read and summed");
-
-//    	System.out.println("Size: "+ streets.size());
-
-    	SortedMap<String, Integer> sorted = new TreeMap<String, Integer>(new Comparator<String>() {
-			public int compare(String o1, String o2) {
-				int c = get(o2) - get(o1);
-				if (c != 0) return c;
-				return o2.compareTo(o1);
-			}});
-
-    	for (int i = 0; i < SIZE; i++) {
-    		for (int v; (v = vals.get(i)) != 0; ) {
-    			sorted.put(keys[i], v);
-    		}
-    	}
-
-    	printInterval("Populated TreeSet");
-
-        return sorted;
-    }
-
     static volatile long lastTime = System.currentTimeMillis();
 
     public static void printInterval(String name) {
     	long time = System.currentTimeMillis();
-    	System.out.println(time +" "+ name +": "+ (time - lastTime) +" ms");
+    	println(name +": "+ (time - lastTime) +" ms");
     	lastTime = time;
     }
 
     public static void printElement(String key, Map<String, Integer> streets) {
-    	System.out.println(key +": $"+ streets.get(key));
+    	println(key +": $"+ streets.get(key));
     }
 
     public static void printProperty(String name) {
-		System.out.println(name +": "+ System.getProperty(name));
+		println(name +": "+ System.getProperty(name));
+    }
+
+    public static void println(String line) {
+    	System.out.println(line);
     }
 }
