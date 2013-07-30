4cc97b8a57c01257fafbd6860237462f35f2e380
Keith Kim <kkim@oanda.com>
2013-07-30 02:23:42 -0400

    Use double-sized AtomicIntegerArray for alternating hash/value

--- src/main/java/ca/kijiji/contest/OpenStringIntHashMap.java
+++ src/main/java/ca/kijiji/contest/OpenStringIntHashMap.java
@@ -13,9 +13,8 @@ public class OpenStringIntHashMap {
 	public final int NO_ELEMENT_VALUE = 0;
 
 	private final int capacity;
-	private final int[] hashes;
 	private final String[] keys;
-	private final AtomicIntegerArray values;
+	private final AtomicIntegerArray hashAndValues;
 
 	private final HashFunction murmur3 = Hashing.murmur3_32();
 
@@ -23,9 +22,8 @@ public class OpenStringIntHashMap {
 
 	public OpenStringIntHashMap(int capacity) {
 		this.capacity = capacity;
-		hashes = new int[capacity];
 		keys = new String[capacity];
-		values = new AtomicIntegerArray(capacity);
+		hashAndValues = new AtomicIntegerArray(2*capacity);
 		pad7 = pad6 = pad5 = pad4 = pad3 = pad2 = pad1 = 7;
 		Pad1 = Pad2 = Pad3 = Pad4 = Pad5 = Pad6 = Pad7 = 7;
 	}
@@ -35,7 +33,27 @@ public class OpenStringIntHashMap {
 		int i0 = hash % capacity;
 		if (i0 < 0) i0 += capacity;
 
-		return get(hash, i0, true);
+		final int i2 = i0 * 2;
+
+		for (int i = i2; i < capacity * 2; i += 2) {
+			int h = hashAndValues.get(i);
+			if (h == hash) {
+				return hashAndValues.get(i + 1);
+			}
+			else if (h == 0) {
+				return NO_ELEMENT_VALUE;
+			}
+		}
+		for (int i = 0; i < i2; i += 2) {
+			int h = hashAndValues.get(i);
+			if (h == hash) {
+				return hashAndValues.get(i + 1);
+			}
+			else if (h == 0) {
+				return NO_ELEMENT_VALUE;
+			}
+		}
+		return NO_ELEMENT_VALUE;
 	}
 
 	public void put(String key, int value) {
@@ -43,7 +61,35 @@ public class OpenStringIntHashMap {
 		int i0 = hash % capacity;
 		if (i0 < 0) i0 += capacity;
 
-		put(key, hash, value, i0, true);
+		final int i2 = i0 * 2;
+
+		for (int i = i2; i < capacity * 2; i += 2) {
+			if (hashAndValues.get(i) == hash) {
+				hashAndValues.set(i + 1, value);
+				return;
+			}
+			else if (hashAndValues.compareAndSet(i, 0, hash)) {
+				hashAndValues.set(i + 1, value);
+				synchronized (keys) {
+					keys[i/2] = key;
+				}
+				return;
+			}
+		}
+		for (int i = 0; i < i2; i += 2) {
+			if (hashAndValues.get(i) == hash) {
+				hashAndValues.set(i + 1, value);
+				return;
+			}
+			else if (hashAndValues.compareAndSet(i, 0, hash)) {
+				hashAndValues.set(i + 1, value);
+				synchronized (keys) {
+					keys[i/2] = key;
+				}
+				return;
+			}
+		}
+		throw new IllegalStateException("Exceeded capacity "+ capacity);
 	}
 
 	public void adjustOrPutValue(String key, int value) {
@@ -51,15 +97,43 @@ public class OpenStringIntHashMap {
 		int i0 = hash % capacity;
 		if (i0 < 0) i0 += capacity;
 
-		adjustOrPutValue(key, hash, value, i0, true);
+		final int i2 = i0 * 2;
+
+		for (int i = i2; i < capacity * 2; i += 2) {
+			if (hashAndValues.get(i) == hash) {
+				hashAndValues.addAndGet(i + 1, value);
+				return;
+			}
+			else if (hashAndValues.compareAndSet(i, 0, hash)) {
+				hashAndValues.addAndGet(i + 1, value);
+				synchronized (keys) {
+					keys[i/2] = key;
+				}
+				return;
+			}
+		}
+		for (int i = 0; i < i2; i += 2) {
+			if (hashAndValues.get(i) == hash) {
+				hashAndValues.addAndGet(i + 1, value);
+				return;
+			}
+			else if (hashAndValues.compareAndSet(i, 0, hash)) {
+				hashAndValues.addAndGet(i + 1, value);
+				synchronized (keys) {
+					keys[i/2] = key;
+				}
+				return;
+			}
+		}
+		throw new IllegalStateException("Exceeded capacity "+ capacity);
 	}
 
 	public void putAllTo(Map<String, Integer> dest) {
-		synchronized (hashes) {
+		synchronized (keys) {
 			int i = 0;
 			for (String key : keys) {
 				if (key != null) {
-					dest.put(key, values.get(i));
+					dest.put(key, hashAndValues.get(i*2 + 1));
 				}
 				i++;
 			}
@@ -68,7 +142,7 @@ public class OpenStringIntHashMap {
 
 	private int hash(String key) {
 		try {
-			int h = murmur3.hashBytes(key.replace(" ", "").getBytes("UTF-8")).asInt();
+			int h = murmur3.hashBytes(key.getBytes("UTF-8")).asInt();
 			if (h == 0) h = capacity;
 			return h;
 		}
@@ -76,125 +150,5 @@ public class OpenStringIntHashMap {
 			e.printStackTrace();
 			return 0;
 		}
-	}
-
-	private int get(int hash, int i0, boolean recurse) {
-		for (int i = i0; i < capacity; i++) {
-			int hi = hashes[i];
-			if (hash == hi) {
-				return values.get(i);
-			}
-			if (hi == 0) {
-				if (recurse) {
-					synchronized (hashes) {
-						return get(hash, i0, false);
-					}
-				}
-				return NO_ELEMENT_VALUE;
-			}
-		}
-		for (int i = 0; i < i0; i++) {
-			int hi = hashes[i];
-			if (hash == hi) {
-				return values.get(i);
-			}
-			if (hi == 0) {
-				if (recurse) {
-					synchronized (hashes) {
-						return get(hash, i0, false);
-					}
-				}
-				return NO_ELEMENT_VALUE;
-			}
-		}
-		return NO_ELEMENT_VALUE;
-	}
-
-	public void put(String key, int hash, int value, int i0, boolean recurse) {
-		for (int i = i0; i < capacity; i++) {
-			int hi = hashes[i];
-			if (hash == hi) {
-				values.set(i, value);
-				return;
-			}
-			if (hi == 0) {
-				if (recurse) {
-					synchronized (hashes) {
-						put(key, hash, value, i, false);
-					}
-				}
-				else {
-					hashes[i] = hash;
-					keys[i] = key;
-					values.set(i, value);
-				}
-				return;
-			}
-		}
-		for (int i = 0; i < i0; i++) {
-			int hi = hashes[i];
-			if (hash == hi) {
-				values.set(i, value);
-				return;
-			}
-			if (hi == 0) {
-				if (recurse) {
-					synchronized (hashes) {
-						put(key, hash, value, i, false);
-					}
-				}
-				else {
-					hashes[i] = hash;
-					keys[i] = key;
-					values.set(i, value);
-				}
-				return;
-			}
-		}
-		throw new IllegalStateException("Exceeded capacity "+ capacity);
-	}
-
-	private void adjustOrPutValue(String key, int hash, int value, int i0, boolean recurse) {
-		for (int i = i0; i < capacity; i++) {
-			int hi = hashes[i];
-			if (hash == hi) {
-				values.addAndGet(i, value);
-				return;
-			}
-			if (hi == 0) {
-				if (recurse) {
-					synchronized (hashes) {
-						put(key, hash, value, i, false);
-					}
-				}
-				else {
-					hashes[i] = hash;
-					keys[i] = key;
-					values.addAndGet(i, value);
-				}
-				return;
-			}
-		}
-		for (int i = 0; i < i0; i++) {
-			int hi = hashes[i];
-			if (hash == hi) {
-				values.set(i, value);
-				return;
-			}
-			if (hi == 0) {
-				if (recurse) {
-					synchronized (hashes) {
-						put(key, hash, value, i, false);
-					}
-				}
-				else {
-					hashes[i] = hash;
-					keys[i] = key;
-					values.addAndGet(i, value);
-				}
-				return;
-			}
-		}
-		throw new IllegalStateException("Exceeded capacity "+ capacity);
 	}
 }
--- src/main/java/ca/kijiji/contest/ParkingTicketsStats.java
+++ src/main/java/ca/kijiji/contest/ParkingTicketsStats.java
@@ -33,8 +33,8 @@ public class ParkingTicketsStats {
 
 	static final Pattern namePattern = Pattern.compile("([A-Z][A-Z][A-Z]+|ST [A-Z][A-Z][A-Z]+)");
 
-    static final int nWorkers = 7;
-	static final ArrayBlockingQueue<Long> thequeue = new ArrayBlockingQueue<>(2048, true);
+    static final int nWorkers = 4;
+	static final ArrayBlockingQueue<Long> thequeue = new ArrayBlockingQueue<>(2 * nWorkers, true);
     static final OpenStringIntHashMap themap = new OpenStringIntHashMap(20480); // 8770
 
     public static SortedMap<String, Integer> sortStreetsByProfitability(final InputStream parkingTicketsStream) {
@@ -56,27 +56,27 @@ public class ParkingTicketsStats {
 	        	workers[t].start();
 	        }
 
-    		int a = 0;
-    		int i = 0;
-    		int j = 0;
+    		int read_end = 0;
+    		int block_start = 0;
+    		int block_end = 0;
     		int t = 0;
-    		for (int c = 4 * 1024 * 1024; (c = parkingTicketsStream.read(data, a, c)) > 0; ) {
-    			a += c;
-    			i = j;
-    			j = a;
-    			if (a < available) {
-    				while (data[--j] != '\n') {}
-        			j++;
+    		for (int read_amount = 1024 * 1024; (read_amount = parkingTicketsStream.read(data, read_end, read_amount)) > 0; ) {
+    			read_end += read_amount;
+    			block_start = block_end;
+    			block_end = read_end;
+    			if (read_end < available) {
+    				while (data[--block_end] != '\n') {}
+        			block_end++;
     			}
 
     			// don't offer the first (header) row
-    			if (i == 0) {
-    		    	printInterval("Local initialization: read first "+ a +" bytes");
+    			if (block_start == 0) {
+    		    	printInterval("Local initialization: read first "+ read_end +" bytes");
 
-    				while (data[i++] != '\n') {};
+    				while (data[block_start++] != '\n') {};
     			}
 
-				final long ij = (long)i << 32 | (long)j & 0x00ffffffff;
+				final long ij = (long)block_start << 32 | (long)block_end & 0x00ffffffff;
 				try {
 					thequeue.put(ij);
 				}
@@ -85,12 +85,12 @@ public class ParkingTicketsStats {
 				}
     			t = (++t) % nWorkers;
 
-    			if (available - a < c) {
-    				c = available - a;
+    			if (available - read_end < read_amount) {
+    				read_amount = available - read_end;
     			}
     		}
 
-	    	printInterval("Local initialization: read remaining of "+ a +" total bytes");
+	    	printInterval("Local initialization: read remaining of "+ read_end +" total bytes");
 
 	        for (t = 0; t < nWorkers; t++) {
 				try {
