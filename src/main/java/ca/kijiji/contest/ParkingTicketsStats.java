f5c9e5e5d07974b06cd395ef6d754b9ff4018785
King Chung Huang <kinghuang@mac.com>
2013-07-29 21:21:35 -0600

    Completed first implementation of ParkingTicketsStats. Success!

--- src/main/java/ca/kijiji/contest/ParkingTicketsStats.java
+++ src/main/java/ca/kijiji/contest/ParkingTicketsStats.java
@@ -1,11 +1,41 @@
 package ca.kijiji.contest;
 
+import java.io.IOException;
 import java.io.InputStream;
+import java.util.HashMap;
 import java.util.SortedMap;
 
 public class ParkingTicketsStats {
 
     public static SortedMap<String, Integer> sortStreetsByProfitability(InputStream parkingTicketsStream) {
-        return null;
-    }
+        ParkingTagsReader reader = new ParkingTagsReader(parkingTicketsStream, false);
+        ParkingTagData data = new ParkingTagData();
+        HashMap<String, Integer> unsortedProfitabilityByStreet = new HashMap<String, Integer>();
+        
+        try {
+        	reader.start();
+        } catch (IOException ioe) {
+        	return null;
+        }
+        
+        while (reader.readTag(data)) {
+        	String streetName = data.streetNameFromLocation2();
+        	
+        	if (streetName != null) {
+        		Integer totalFine = unsortedProfitabilityByStreet.get(streetName);
+        		if (totalFine == null) {
+        			totalFine = new Integer(data.set_fine_amount);
+        		} else {
+        			totalFine = totalFine + Integer.decode(data.set_fine_amount);
+        		}
+        		unsortedProfitabilityByStreet.put(streetName, totalFine);
+        	}
+        }
+        
+        SortedMapByValue<String, Integer> sortedProfitabilityByStreet = new SortedMapByValue<String, Integer>();
+        sortedProfitabilityByStreet.entrySet().addAll(unsortedProfitabilityByStreet.entrySet());
+        
+        return sortedProfitabilityByStreet;
+    } 
+    
 }
