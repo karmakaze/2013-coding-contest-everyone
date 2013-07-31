f784acbcf0657d1a62b7957665c03cbb32f1b407
King Chung Huang <kinghuang@mac.com>
2013-07-31 17:02:03 -0600

    Added comments and logging.

--- src/main/java/ca/kijiji/contest/ParkingTicketsStats.java
+++ src/main/java/ca/kijiji/contest/ParkingTicketsStats.java
@@ -1,5 +1,8 @@
 package ca.kijiji.contest;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.BufferedReader;
 import java.io.CharArrayReader;
 import java.io.IOException;
@@ -18,32 +21,40 @@ import java.util.concurrent.TimeUnit;
 
 public class ParkingTicketsStats {
 
-	public enum ThreadingScheme {
+	enum ThreadingScheme {
 		SingleThreaded,
 		MultiThreaded
 	}
 	
-	public enum ParsingScheme {
+	enum ParsingScheme {
 		Regex,
 		Scanning,
 		Splitting
 	}
+
+	final static Logger LOG = LoggerFactory.getLogger(ParkingTicketsStats.class);
 	
 	final static boolean parseSignificantDataOnly = true;
 	final static int dataChunkSize = 10 * 1024 * 1024;
 	
-	final static ThreadingScheme threadingScheme = ThreadingScheme.MultiThreaded;
+	final static ThreadingScheme threadingScheme = ThreadingScheme.SingleThreaded;
 	final static ParsingScheme parsingScheme = ParsingScheme.Splitting;
 
     public static SortedMap<String, Integer> sortStreetsByProfitability(InputStream parkingTicketsStream) {
     	BufferedReader parkingTicketsReader = null;
     	
+    	// Wrap parkingTicketsStream in a BufferedReader and read (throw away) the header line
     	try {
         	parkingTicketsReader = new BufferedReader(new InputStreamReader(parkingTicketsStream, "ascii"));
         	parkingTicketsReader.readLine();
         } catch (IOException ioe) {
+        	LOG.error("An error occurred creating parkingTicketsReader and reading the header line.");
+        	
         	return null;
         }
+    	
+    	// Using the specified threading scheme, get an unsorted map of streets and their profitability
+    	LOG.info("Sorting parking tickets using the {} and {} schemes.", threadingScheme, parsingScheme);
     	
     	Map<String, Integer> unsortedProfitabilityByStreet = null;
     	
@@ -60,8 +71,14 @@ public class ParkingTicketsStats {
     			throw new UnsupportedOperationException();
     	}
     	
+    	// Sort the street and profitability data and return
+    	long sortStartTime = System.currentTimeMillis();
+    	
     	SortedMapByValue<String, Integer> sortedProfitabilityByStreet = new SortedMapByValue<String, Integer>();
         sortedProfitabilityByStreet.entrySet().addAll(unsortedProfitabilityByStreet.entrySet());
+        
+        long sortDuration = System.currentTimeMillis() - sortStartTime;
+        LOG.info("Duration of sorting = {} ms", sortDuration);
         
     	return sortedProfitabilityByStreet;
     }
@@ -76,6 +93,8 @@ public class ParkingTicketsStats {
      * @return
      */
     static Map<String, Integer> streetsByProfitabilityUsingSingleThread(BufferedReader parkingTicketsReader) {
+    	long procStartTime = System.currentTimeMillis();
+    	
     	Callable<HashMap<String, Integer>> processor = new TagDataChunkProcessor(parkingTicketsReader);
     	HashMap<String, Integer> unsortedProfitabilityByStreet = null;
 		try {
@@ -83,12 +102,15 @@ public class ParkingTicketsStats {
 		} catch (Exception e) {
 			return null;
 		}
+		
+		long procDuration = System.currentTimeMillis() - procStartTime;
+        LOG.info("Duration of data reading and processing = {} ms", procDuration);
     	
         return unsortedProfitabilityByStreet;
     }
     
      /**
-     * MultiThreadedWithRegex Approach
+     * MultiThreaded Approach
      * 
      * This approach reads in chars in approximately 10 MB chunks (always ending at the end of a line)
      * on the main thread, and uses an ExecutorService to manage TagDataChunkProcessor instances.
@@ -110,19 +132,29 @@ public class ParkingTicketsStats {
     	int charactersRead = -1;
     	int remainingLength = 0;
     	
+    	// Read in parkingTickets data in dataChunkSize chunks, then create TagDataChunkProcessor
+    	// instances for each chunk and dispatched them to the consumers ExecutorService.
+    	long procStartTime = System.currentTimeMillis();
+    	
     	try {
     		do {
+    			// dataChunk is initialized with space for an extra 256 characters
+    			// to cover reading to the end of a line after dataChunkSize characters
+    			// are read. That way each chunk ends with a complete line.
+    			// Lines appear to be less than 100 characters long, normally.
     			dataChunk = new char[dataChunkSize + 256];
     			charactersRead = parkingTicketsReader.read(dataChunk, 0, dataChunkSize);
     			
     			if (charactersRead != -1) {
     				remainingLine = parkingTicketsReader.readLine();
     				
+    				// Read to the end of a line and append it to dataChunk, if necessary
     				if (remainingLine != null && (remainingLength = remainingLine.length()) > 0) {
     					remainingLine.getChars(0, remainingLength, dataChunk, charactersRead);
     					charactersRead += remainingLength;
     				}
     				
+    				// Create a TagDataChunkProcessor and submit it to the ExecutorService for running
     				dataChunkReader = new BufferedReader(new CharArrayReader(dataChunk, 0, charactersRead));
     				futures.add(consumers.submit(new TagDataChunkProcessor(dataChunkReader)));
     			}
@@ -131,12 +163,22 @@ public class ParkingTicketsStats {
     		ioe.printStackTrace();
     	}
     	
+    	long chunkDuration = System.currentTimeMillis() - procStartTime;
+        LOG.info("Duration of data chunking = {} ms", chunkDuration);
+    	
+        // Stop accepting new dispatches and wait for all the chunk processors to finish
     	try {
     		consumers.shutdown();
 			consumers.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
+    	
+    	long procDuration = System.currentTimeMillis() - procStartTime;
+        LOG.info("Duration of data chunking and processing = {} ms", procDuration);
+    	
+    	// Reduce the intermediate results from each TagDataChunkProcessor via their futures
+    	long reduceStartTime = System.currentTimeMillis();
     	
     	HashMap<String, Integer> futureProfitabilityByStreet = null;
     	HashMap<String, Integer> accumulatedProfitabilityByStreet = new HashMap<String, Integer>();
@@ -158,6 +200,9 @@ public class ParkingTicketsStats {
     	} catch (ExecutionException e) {
     		e.printStackTrace();
     	}
+    	
+    	long reduceDuration = System.currentTimeMillis() - reduceStartTime;
+        LOG.info("Duration of reduce = {} ms", reduceDuration);
     	
     	return accumulatedProfitabilityByStreet;
     }
