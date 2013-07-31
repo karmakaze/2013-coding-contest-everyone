495342f625165cb0eecf94ee0ed85a7024937e66
William Howard <whoward.tke@gmail.com>
2013-07-31 11:05:26 -0400

    update counts, try to use 2.5m

--- perf/address.rb
+++ perf/address.rb
@@ -5,14 +5,14 @@ require 'address'
 
 Benchmark.bmbm do |x|
    x.report("#new") do
-      1_000_000.times { Address.new("300 KING STREET W") }
+      2_500_000.times { Address.new("300 KING STREET W") }
    end
 
    x.report("#new + #tokens") do
-      1_000_000.times { Address.new("300 KING STREET W").tokens }
+      2_500_000.times { Address.new("300 KING STREET W").tokens }
    end
 
    x.report("#new + #data") do
-      1_000_000.times { Address.new("300 KING STREET W").data }
+      2_500_000.times { Address.new("300 KING STREET W").data }
    end
 end
